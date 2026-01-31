8ec4655c0c387b7158119021a75d9986e037caca
denis-papin <denis1@fastmail.com>
2026-01-31 18:48:52 +0100

    v1.0

--- README.md
+++
@@ -1,2 +0,0 @@
-# ravenx
-ravenx
---
+++ chapters.js
@@ -0,0 +1,356 @@
+// ========= DONNÉES =========
+const chapters = [
+
+    // ======================================================
+    // 00-poster.jpg
+    {
+        title: "Raven X — L'âme éternelle",
+        blocks: [
+            {
+                image: "00-poster.jpg",
+                text: "Dans un futur lointain, l’humanité fait face à la plus grande menace de son histoire.\n" +
+                    "Une armada venue des étoiles s’abat sur la Terre.\n" +
+                    "\n" +
+                    "Alors que les villes tombent et que l’espoir s’éteint, un projet secret voit le jour : l’Arme X.\n" +
+                    "\n" +
+                    "Mais la guerre ne se gagnera pas seulement par la force.\n" +
+                    "Entre la perte, la peur et le doute, c’est le lien entre une âme humaine et une machine de combat qui décidera du destin de la Terre.\n" +
+                    "\n" +
+                    "La force d’une jeune fille et le courage de tous seront-ils suffisants " +
+                    "pour vaincre… ou l’humanité est-elle déjà condamnée ?",
+
+                orientation: "image-left"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 05-invasion.jpg
+    {
+        title: "Chapitre 1 — L'Invasion",
+        blocks: [
+            {
+                image: "05-invasion.jpg",
+                text: "Terre, 2149.\n" +
+                    "Ils arrivent.\n" +
+                    "Une horde d’envahisseurs surgie des confins de la galaxie, innombrable et implacable.\n" +
+                    "Le ciel de l’espace s’assombrit sous leurs vaisseaux.\n" +
+                    "La fin d’un monde est en marche.",
+                orientation: "image-right"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 07-allegence.jpg
+    {
+        title: "Chapitre 2 — Allégeance",
+        blocks: [
+            {
+                image: "07-allegence.jpg",
+                text: "Zaryth, roi démon et architecte de l’invasion, domine son royaume comme un dieu de destruction.\n" +
+                    "Son corps, forgé de métal vivant et de flammes, irradie une puissance ancienne, inhumaine. Chaque fissure de son armure semble contenir un cœur en fusion.\n" +
+                    "\n" +
+                    "À ses pieds, Sarym s’agenouille.\n" +
+                    "Non par faiblesse, mais par choix.\n" +
+                    "Son frère cadet prête allégeance, l’arme plantée dans la pierre brûlante, scellant " +
+                    "un pacte de sang et de conquête. Ensemble, ils ne sont pas seulement des chefs de guerre : " +
+                    "ils sont la volonté même de l’anéantissement.",
+                orientation: "image-left"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 08-insouciance.jpg
+    {
+        title: "Chapitre 3 — L'Insouciance",
+        blocks: [
+            {
+                image: "08-insouciance.jpg",
+                text: "Il y a seulement quelques mois encore, la vie s’écoulait paisiblement.\n" +
+                    "Sur le campus baigné de lumière, Yuki avançait aux côtés de ses amis, les bras chargés de livres, portée par l’insouciance de la jeunesse.\n" +
+                    "Les rires résonnaient entre les bâtiments, les projets semblaient infinis, et l’avenir paraissait ouvert.\n" +
+                    "\n" +
+                    "À cet instant, nul ne pouvait imaginer que ce quotidien simple et heureux touchait déjà à sa fin.",
+                orientation: "image-right"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 09-destin.jpg
+    {
+        title: "Chapitre 4 — Le Destin",
+        blocks: [
+            {
+                image: "09-destin.jpg",
+                text: "Un jour d’hiver, les autorités vinrent à la rencontre de Yuki.\n" +
+                    "Elles lui révélèrent l’existence d’une mission capitale, née du désespoir et de l’urgence.\n" +
+                    "Une mission qu’elle seule était en mesure d’assumer : L'Arme X. " +
+                    "Son profil génétique faisait d'elle la personne idéale pour mener le projet à bien.",
+                orientation: "image-left"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 10-depart.jpg
+    {
+        title: "Chapitre 5 — Le Départ",
+        blocks: [
+            {
+                image: "10-depart.jpg",
+                text: "La séparation avec les êtres chers fut douloureuse.\n" +
+                    "Dire adieu à celui qu’elle aimait revenait à abandonner tout ce qu’elle avait été jusque-là.\n" +
+                    "Pourtant, la menace dont elle venait d’apprendre l’existence ne lui laissait aucun choix.\n" +
+                    "\n" +
+                    "Ce jour-là, Yuki accepta de disparaître en tant que personne,\n" +
+                    "pour devenir ce que le monde attendait d’elle : une arme.",
+                orientation: "image-right"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 20-transformation.jpg
+    {
+        title: "Chapitre 6 — La Transformation",
+        blocks: [
+            {
+                image: "20-transformation.jpg",
+                text: "Dans le silence froid du laboratoire, Yuki s’endormit.\n" +
+                    "Autour d’elle, des câbles et des interfaces s’animèrent lentement, épousant les contours de son corps immobile.\n" +
+                    "\n" +
+                    "Une lumière bleue se mit à circuler dans l’armure, douce et inquiétante à la fois.\n" +
+                    "Elle n’était ni feu ni électricité, mais le signe d’un lien en train de naître —\n" +
+                    "une énergie nouvelle, destinée à porter une volonté humaine dans une machine de guerre.\n" +
+                    "\n" +
+                    "À mesure que l’armure se refermait, le monde de Yuki s’éloignait.\n" +
+                    "Ce n’était pas seulement son corps qui fusionnait,\n" +
+                    "mais son esprit, ses souvenirs… et son courage. Raven X était née.",
+                orientation: "image-left"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 30-rage.jpg
+    {
+        title: "Chapitre 7 — La Rage",
+        blocks: [
+            {
+                image: "30-rage.jpg",
+                text: "Lorsque Raven X fut déployée pour la première fois, le ciel était déjà en feu.\n" +
+                    "Les armées de l’envahisseur déferlaient sur les villes, froides et méthodiques, laissant derrière elles ruines et silence.\n" +
+                    "\n" +
+                    "Au cœur du chaos, l’armure RX s’éveilla.\n" +
+                    "L’énergie bleue guidait ses mouvements" +
+                    " qui portaient la force d’une machine… mais aussi la volonté de Yuki...",
+                orientation: "image-right"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 32-brutal.jpg
+    {
+        title: "Chapitre 8 — Brutalité",
+        blocks: [
+            {
+                image: "32-brutal.jpg",
+                text: "...Les combats furent violents, incessants.\n" +
+                    "Acier contre acier, lumière contre ténèbres.\n" +
+                    "Et tandis que les robots ennemis tombaient, l’humanité reprenait lentement espoir.\n" +
+                    "\n" +
+                    "Mais à chaque victoire, Yuki s’éloignait un peu plus de celle qu’elle avait été.",
+                orientation: "image-left"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 35-destructions.jpg
+    {
+        title: "Chapitre 9 — Destructions",
+        blocks: [
+            {
+                image: "35-destructions.jpg",
+                text: "Les machines ennemies, toujours plus nombreuses et plus puissantes, tentèrent d’opposer leur résistance.\n" +
+                    "Mais aucune ne put tenir face au laser bleu de RX.",
+                orientation: "image-right"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 50-prisonnière.jpg
+    {
+        title: "Chapitre 10 — Prisonnière",
+        blocks: [
+            {
+                image: "50-prisonnière.jpg",
+                text: "Mais les succès remportés au fil des combats allaient bientôt rencontrer un coup d’arrêt.\n" +
+                    "Face à Sarym, les victoires passées perdaient leur sens.\n" +
+                    "Là où les armées de l’envahisseur avaient cédé à la force de Raven X,\n" +
+                    "Sarym opposa une puissance plus sournoise, plus cruelle encore.\n" +
+                    "\n" +
+                    "Ce n’était plus seulement la machine qu’il affrontait,\n" +
+                    "mais le lien qui unissait Yuki à l’armure.\n" +
+                    "Et pour la première fois depuis le début de la guerre,\n" +
+                    "l’issue du combat devint incertaine." +
+                    "Sarym emprisonna Raven X dans ses câbles bio-énergétiques,\n" +
+                    "tentant de briser le lien fragile qui l’unissait à Yuki.\n" +
+                    "Chaque pulsation des entraves résonnait comme une attaque directe contre son esprit,\n" +
+                    "cherchant à séparer la machine de l’âme qui la guidait.",
+                orientation: "image-left"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 60-rebellion.jpg
+    {
+        title: "Chapitre 11 — La Rébellion",
+        blocks: [
+            {
+                image: "60-rebellion.jpg",
+                text: "Contre toute attente, l’énergie bleue se propagea au cœur même de Raven X,\n" +
+                    "envahissant son âme plutôt que de la consumer.\n" +
+                    "Ce qui devait être brisé se renforça.\n" +
+                    "Ce qui devait céder se transforma.\n" +
+                    "\n" +
+                    "Libérée de toute retenue, une puissance incontrôlée s’éveilla,\n" +
+                    "née de la fusion totale entre la machine et la volonté de Yuki.\n" +
+                    "À cet instant, Raven X ne fut plus seulement une arme…\n" +
+                    "mais une force que rien ne pouvait désormais contenir.",
+                orientation: "image-right"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 70-interior-chat.jpg
+    {
+        title: "Chapitre 12 — Doutes",
+        blocks: [
+            {
+                image: "70-interior-chat.jpg",
+                text: "Le déchaînement d’énergie incontrôlée qui consuma Sarym emporta la victoire…\n" +
+                    "mais dans un cruel effet de balancier, il vida également Raven X de ses forces.\n" +
+                    "\n" +
+                    "La bataille était gagnée,\n" +
+                    "pourtant la victoire avait le goût amer d’une défaite.\n" +
+                    "Une victoire à la Pyrrhus, payée au prix de l’épuisement total.\n" +
+                    "\n" +
+                    "Dans le silence de l’espace intérieur,\n" +
+                    "l’âme de Yuki, devenue spectrale, se retrouva seule face à l’armure exsangue.\n" +
+                    "Privée de mouvement, privée de voix,\n" +
+                    "Raven X n’était plus qu’un armure vidée de son énergie.\n" +
+                    "\n" +
+                    "Et pourtant, au cœur de cette immobilité,\n" +
+                    "le lien subsistait encore." +
+                    "Il ne pouvait y avoir de défaite,\n" +
+                    "pas maintenant,\n" +
+                    "pas si près de la victoire totale.",
+                orientation: "image-left"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 80-deluge.jpg
+    {
+        title: "Chapitre 13 — Le Déluge",
+        blocks: [
+            {
+                image: "80-deluge.jpg",
+                text: "Après son retour sur Terre, Raven X, aidée par l’équipe qui l’avait créée,\n" +
+                    "subit les modifications essentielles à sa survie,\n" +
+                    "ainsi que les évolutions nécessaires à l’ultime affrontement.\n" +
+                    "\n" +
+                    "Renforcée, transformée,\n" +
+                    "elle retourna sur le champ de bataille.\n" +
+                    "\n" +
+                    "Face à elle, l’armada de l’envahisseur ne trouva plus aucune échappatoire.",
+                orientation: "image-right"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 85-mise à mort.jpg
+    {
+        title: "Chapitre 14 — La Mise à Mort",
+        blocks: [
+            {
+                image: "85-mise à mort.jpg",
+                text: "Après le déluge, le roi démon se retira sur la lune. " +
+                    "Face au Héraut de la Terre, il déchaîna sa fureur,\n" +
+                    "chaque impact ébranlant la poussière lunaire.\n" +
+                    "Mais cette fois, la force brute ne suffisait plus.\n" +
+                    "\n" +
+                    "Au fil du combat, l’énergie bleue se stabilisa.\n" +
+                    "Canalisée, contenue, elle répondit avec précision.\n" +
+                    "Raven X apprit à la modeler, non plus comme une arme aveugle,\n" +
+                    "mais comme un instrument de justice.\n" +
+                    "\n" +
+                    "Dans un dernier instant suspendu,\n" +
+                    "l’énergie se condensa entre ses mains,\n" +
+                    "prenant la forme d’un sabre d’énergie pure,\n" +
+                    "tranchant, silencieux, absolu.\n" +
+                    "\n" +
+                    "Un seul geste.\n" +
+                    "Dans l’immobilité de l’espace,\n" +
+                    "le mal fut décapité.\n" +
+                    "\n" +
+                    "La guerre était terminée.",
+                orientation: "image-left"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 90-victoire.jpg
+    {
+        title: "Chapitre 15 — Victoire",
+        blocks: [
+            {
+                image: "90-victoire.jpg",
+                text:
+                    "Raven X retourna sur Terre,\n" +
+                    "portant avec elle son funeste trophée,\n" +
+                    "preuve silencieuse de la chute du mal.\n" +
+                    "\n" +
+                    "À sa vue, les armes se turent,\n" +
+                    "les cieux s’éclaircirent,\n" +
+                    "et l’humanité comprit que l’invasion avait pris fin.",
+                orientation: "image-right"
+            }
+        ]
+    },
+
+    // ======================================================
+    // 100-nouvel espoir.jpg
+    {
+        title: "Épilogue — Un Nouvel Espoir",
+        blocks: [
+            {
+                image: "100-nouvel espoir.jpg",
+                text: "L’énergie bleue, désormais sans but, quitta lentement l’armure.\n" +
+                    "\n" +
+                    "Yuki en fut libérée.\n" +
+                    "\n" +
+                    "Ce qui avait été une arme redevint un corps,\n" +
+                    "ce qui avait été un sacrifice laissa place à la vie.\n" +
+                    "La guerre avait pris beaucoup,\n" +
+                    "mais elle n’avait pas tout emporté.\n" +
+                    "\n" +
+                    "Pour Yuki,\n" +
+                    "un nouvel espoir venait de naître. Une nouvelle vie.",
+                orientation: "image-left"
+            }
+        ]
+    }
+
+];
